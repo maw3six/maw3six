db623b8d9209c1b44c9156e2d8a151555661db1c
Maw <adikris00@protonmail.com>
2026-07-24 16:55:31 +0700

    Update Log1n.php

--- Log1n.php
+++ Log1n.php
@@ -60,7 +60,7 @@ function show_login_page($message = "")
 }
 
 if (!isset($_SESSION['authenticated'])) {
-    $stored_hashed_password = '$2a$12$VPJy6kC1L6EIVQaZWHzVTOEQ1AEe7sIhrRBVSEzsjz1DyEnriBcvW'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
+    $stored_hashed_password = '$2a$12$C.XBT5eydZL01GXungU45OgFt.EI06rYEemiRPYMMYPjLo/iuiwzy'; // Use PASSWORD_DEFAULT hash u can use this site https://phppasswordhash.com/ for generate ur password >  Gunakan hash PASSWORD_DEFAULT Anda dapat menggunakan situs ini https://phppasswordhash.com/ untuk menghasilkan kata sandi Anda
 
     if (isset($_POST['pass']) && password_verify($_POST['pass'], $stored_hashed_password)) {
         $_SESSION['authenticated'] = true;
